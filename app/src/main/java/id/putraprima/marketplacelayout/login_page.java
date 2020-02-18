26471a753ee91a963bde53a3a951616745c1eb1a
nzzakkya <n.zakkya2@gmail.com>
2020-02-18 08:09:04 +0700

    Add intent

--- app/src/main/java/id/putraprima/marketplacelayout/login_page.java
+++ app/src/main/java/id/putraprima/marketplacelayout/login_page.java
@@ -2,7 +2,9 @@ package id.putraprima.marketplacelayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class login_page extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class login_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page2);
+    }
+
+    public void clickRegister (View view){
+        Intent intent = new Intent( this, register_page.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/id/putraprima/marketplacelayout/register_page.java
+++ app/src/main/java/id/putraprima/marketplacelayout/register_page.java
@@ -2,7 +2,9 @@ package id.putraprima.marketplacelayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class register_page extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class register_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_page);
+    }
+
+    public void clickPersonal (View view){
+        Intent intent = new Intent( this, personal_info.class);
+        startActivity(intent);
     }
 }
